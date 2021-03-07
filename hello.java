b96f27f4b08e07afec69523d7b9e804c8c3a158f
billah1 <billah9578@gmail.com>
2021-03-07 13:23:54 +0600

    added a new display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
     public static void main(string[args]){
         system.out.println("this is java coed");
+        greeetin("masum billah");
+    }
+    public static void greeetin (string name){
+        system.out.println("welcome" +name);
     }
 }
